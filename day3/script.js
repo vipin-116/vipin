27f6ee815aafd8b10c704da99cebc19085f90521
HP <vipinak.23eie@kongu.edu>
2025-06-20 09:45:37 +0530

    day3 added

---
+++ day3/script.js
@@ -0,0 +1,25 @@
+let userScore = 0;
+let computerScore = 0;
+
+function play(userChoice) {
+  const choices = ['stone', 'paper', 'scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (userChoice === computerChoice) {
+    result = "It's a draw!";
+  } else if (
+    (userChoice === "stone" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "stone") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = `You win! ${userChoice} beats ${computerChoice}`;
+    userScore++;
+  } else {
+    result = `You lose! ${computerChoice} beats ${userChoice}`;
+    computerScore++;
+  }
+
+  document.getElementById("result").innerText = result;
+  document.getElementById("score").innerText = `You: ${userScore} | Computer: ${computerScore}`;
+}
